7381ee137f04b83a0be6b39a70cc6b34be4dee5b
Tim Adler <tim@Tims-MBP.fritz.box>
2020-01-31 22:15:33 +0100

    fix

--- lib/huginn_airtable_agent/youtube_dl_agent.rb
+++ lib/huginn_airtable_agent/youtube_dl_agent.rb
@@ -25,8 +25,8 @@ module Agents
 
     def receive(incoming_events)
       incoming_events.each do |event|
-        result = Open3.capture2("youtube-dl", "-j", "https://www.youtube.com/watch?v=IJH_RbnrGUs").&first
-        if result
+        result = Open3.capture2("youtube-dl", "-j", "https://www.youtube.com/watch?v=IJH_RbnrGUs")
+        if result[0]
           payload = boolify(options['merge']) ? event.payload : {}
           payload.merge!(JSON.parse result)
           create_event payload: payload
